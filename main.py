35be0fa7e071f3258d70d3c04f14b5539d30c1c4
adepuuu <ade.santito@gmail.com>
2023-07-08 00:45:20 +0700

    feat: increase nr chunk

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from scipy.signal import iirnotch, lfilter
 
 # Set the chunk size, sample format, channel, sample rate, and duration
 CHUNK = 4*1024
+NR_CHUNK = 500000
 FORMAT = pyaudio.paInt24
 CHANNELS = 1
 RATE = 48000
@@ -77,26 +78,21 @@ def process_and_upload(frames, timestamp):
     # Read and convert the 24-bit audio to 16-bit
     data, rate = sf.read(WAVE_OUTPUT_FILENAME, dtype='float32')
     # We'll now process the audio data in chunks using the noisereduce library
-    # reduced_noise_chunks = []
-    # for i in range(0, len(data), CHUNK):
-    #     chunk_data = data[i:i + CHUNK]
+    reduced_noise_chunks = []
+    for i in range(0, len(data), NR_CHUNK):
+        chunk_data = data[i:i + NR_CHUNK]
 
-    #     # replace 'chunk_data' with the data chunk you want to filter
-    #     filetered_chunk_data = apply_notch_filter(chunk_data, RATE)
+        # replace 'chunk_data' with the data chunk you want to filter
+        filetered_chunk_data = apply_notch_filter(chunk_data, RATE)
 
-    #     # And subsequently replace 'reduced_noise_chunk' with the filtered data before noise reduction
-    #     reduced_noise_chunk = nr.reduce_noise(y=filetered_chunk_data, sr=RATE)
+        # And subsequently replace 'reduced_noise_chunk' with the filtered data before noise reduction
+        reduced_noise_chunk = nr.reduce_noise(y=filetered_chunk_data, sr=RATE)
             
-    #     # Append reduced noise chunk to list
-    #     reduced_noise_chunks.append(reduced_noise_chunk)
+        # Append reduced noise chunk to list
+        reduced_noise_chunks.append(reduced_noise_chunk)
 
-    # # Concatenate all the processed chunks
-    # reduced_noise = np.concatenate(reduced_noise_chunks)
-    # Apply notch filter to the entire data
-    filtered_data = apply_notch_filter(data, RATE)
-
-    # Apply noise reduction to the filtered data
-    reduced_noise = nr.reduce_noise(y=filtered_data, sr=RATE)
+    # Concatenate all the processed chunks
+    reduced_noise = np.concatenate(reduced_noise_chunks)
     reduced_filename = f"reduced-{timestamp}.wav"
 
     # Save the result (Optionally use soundfile library if you want to retain the original file properties)
