0fe4f13853239099f0c88ac255cc26e678dd03bd
adepuuu <ade.santito@gmail.com>
2023-07-08 00:03:13 +0700

    feat: utilizing threading

--- main.py
+++ main.py
@@ -20,9 +20,7 @@ RATE = 48000
 RECORD_SECONDS = 10
 USB_DEVICE_INDEX = 1
 
-# Get the current timestamp and format it as a string
-timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-WAVE_OUTPUT_FILENAME = f"output-{timestamp}.wav"
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -52,6 +50,59 @@ def exit_handler():
 # Register the exit handler to be called when the program is about to exit
 atexit.register(exit_handler)
 
+def process_and_upload(frames, timestamp):
+    # Save the recorded data to a WAV file
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
+    wf.close()
+
+    # Read and convert the 24-bit audio to 16-bit
+    data, rate = sf.read(WAVE_OUTPUT_FILENAME, dtype='float32')
+    # We'll now process the audio data in chunks using the noisereduce library
+    reduced_noise_chunks = []
+    for i in range(0, len(data), CHUNK):
+        chunk_data = data[i:i + CHUNK]
+
+        # Perform noise reduction on the chunk
+        reduced_noise_chunk = nr.reduce_noise(y=chunk_data, sr=rate)
+            
+        # Append reduced noise chunk to list
+        reduced_noise_chunks.append(reduced_noise_chunk)
+
+    # Concatenate all the processed chunks
+    reduced_noise = np.concatenate(reduced_noise_chunks)
+    reduced_filename = f"reduced-{timestamp}.wav"
+
+    # Save the result (Optionally use soundfile library if you want to retain the original file properties)
+    sf.write(reduced_filename, reduced_noise, rate)
+
+    # If the original file exists, delete it
+    if os.path.exists(WAVE_OUTPUT_FILENAME):
+        os.remove(WAVE_OUTPUT_FILENAME)
+        print(f'Successfully deleted local file {WAVE_OUTPUT_FILENAME}')
+
+    # Create a session using your AWS credentials
+    s3 = boto3.resource('s3')
+
+    # Name of the S3 bucket
+    bucket_name = 'audio-guestbook'
+
+    # Try to upload the .wav file to the S3 bucket
+    try:
+        s3.Bucket(bucket_name).upload_file(reduced_filename, reduced_filename)
+        print(f'Successfully uploaded {reduced_filename} to {bucket_name}')
+
+        # If the upload was successful, delete the file
+        if os.path.exists(reduced_filename):
+            os.remove(reduced_filename)
+            print(f'Successfully deleted local file {reduced_filename}')
+
+    except Exception as e:
+        print(f'Failed to upload {reduced_filename} to {bucket_name} due to {e}')
+
 def button_callback(channel):
     global isOpen
     global frames
@@ -60,6 +111,10 @@ def button_callback(channel):
 
     # When the button is pressed, start recording
     if GPIO.input(10): # if pin is HIGH
+        # Get the current timestamp and format it as a string
+        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+        WAVE_OUTPUT_FILENAME = f"output-{timestamp}.wav"
+        
         isOpen.set()
         print("Recording started")
 
@@ -88,59 +143,9 @@ def button_callback(channel):
         stream.close()
         stream = None
 
-        # Save the recorded data to a WAV file
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+        # Start a new thread to process and upload the recording
+        threading.Thread(target=process_and_upload, args=(frames, timestamp)).start()
 
-        # Read and convert the 24-bit audio to 16-bit
-        data, rate = sf.read(WAVE_OUTPUT_FILENAME, dtype='float32')
-
-        # We'll now process the audio data in chunks using the noisereduce library
-        reduced_noise_chunks = []
-
-        for i in range(0, len(data), CHUNK):
-            chunk_data = data[i:i + CHUNK]
-
-            # Perform noise reduction on the chunk
-            reduced_noise_chunk = nr.reduce_noise(y=chunk_data, sr=rate)
-            
-            # Append reduced noise chunk to list
-            reduced_noise_chunks.append(reduced_noise_chunk)
-
-        # Concatenate all the processed chunks
-        reduced_noise = np.concatenate(reduced_noise_chunks)
-        reduced_filename = f"reduced-{timestamp}.wav"
-
-        # Save the result (Optionally use soundfile library if you want to retain the original file properties)
-        sf.write(reduced_filename, reduced_noise, rate)
-
-        # If the original file exists, delete it
-        if os.path.exists(WAVE_OUTPUT_FILENAME):
-            os.remove(WAVE_OUTPUT_FILENAME)
-            print(f'Successfully deleted local file {WAVE_OUTPUT_FILENAME}')
-
-        # Create a session using your AWS credentials
-        s3 = boto3.resource('s3')
-
-        # Name of the S3 bucket
-        bucket_name = 'audio-guestbook'
-
-        # Try to upload the .wav file to the S3 bucket
-        try:
-            s3.Bucket(bucket_name).upload_file(reduced_filename, reduced_filename)
-            print(f'Successfully uploaded {reduced_filename} to {bucket_name}')
-
-            # If the upload was successful, delete the file
-            if os.path.exists(reduced_filename):
-                os.remove(reduced_filename)
-                print(f'Successfully deleted local file {reduced_filename}')
-
-        except Exception as e:
-            print(f'Failed to upload {reduced_filename} to {bucket_name} due to {e}')
         frames = []
 
 # Use BOTH edge detection and increase bouncetime
